2217f19e28c06cddd069907d83cddea8a20d9cdd
Antoine TON THAT <antton-t@student.42.fr>
2023-05-11 18:17:34 +0200

    selection subject ok

--- train.py
+++ train.py
@@ -10,6 +10,7 @@ import matplotlib.pyplot as plt
 from mne.datasets import sample
 from mne.io import concatenate_raws
 import tkinter as tk
+from tkinter import ttk
 
 
 #https://physionet.org/content/eegmmidb/1.0.0/
@@ -56,6 +57,9 @@ def analyse() ->int:
 
     return 0
 
+def selectAll():
+    print("Test")
+
 def main() ->int:
     
     # Create a Tkinter window
@@ -63,34 +67,33 @@ def main() ->int:
     window.title("TotalVortex")
     window.geometry("800x600")
 
-    # Define the options
-    options = ["Option 1", "Option 2", "Option 3"]
+    # Define the option for Subject
+    subjectFont = tk.LabelFrame(window, text="Subject")
+    subjectFont.pack(padx=10, pady=10)
+    subjectVar = tk.StringVar()
+    subjectVar.set("Select the Subject")
+    options = ["All", "Subject 1", "Subject 2", "Subject 3", "Subject 4", "Subject 5", "Subject 6", "Subject 7", "Subject 8", "Subject 9", "Subject 10", "Subject 11", "Subject 12", "Subject 13", "Subject 14", "Subject 15", "Subject 16", "Subject 17", "Subject 18", "Subject 19", "Subject 20", "Subject 21", "Subject 22", "Subject 23", "Subject 24", "Subject 25", "Subject 26", "Subject 27", "Subject 28", "Subject 29", "Subject 30", "Subject 31", "Subject 32", "Subject 33", "Subject 34", "Subject 35", "Subject 36", "Subject 37", "Subject 38", "Subject 39", "Subject 40", "Subject 41", "Subject 42", "Subject 43", "Subject 44", "Subject 45", "Subject 46", "Subject 47", "Subject 48", "Subject 49", "Subject 50", "Subject 51", "Subject 52", "Subject 53", "Subject 54", "Subject 55", "Subject 56", "Subject 57", "Subject 58", "Subject 59", "Subject 60", "Subject 61", "Subject 62", "Subject 63", "Subject 64", "Subject 65", "Subject 66", "Subject 67", "Subject 68", "Subject 69", "Subject 70", "Subject 71", "Subject 72", "Subject 73", "Subject 74", "Subject 75", "Subject 76", "Subject 77", "Subject 78", "Subject 79", "Subject 80", "Subject 81", "Subject 82", "Subject 83", "Subject 84", "Subject 85", "Subject 86", "Subject 87", "Subject 88", "Subject 89", "Subject 90", "Subject 91", "Subject 92", "Subject 93", "Subject 94", "Subject 95", "Subject 96", "Subject 97", "Subject 98", "Subject 99", "Subject 100", "Subject 101", "Subject 102", "Subject 103", "Subject 104", "Subject 105", "Subject 106", "Subject 107", "Subject 108", "Subject 109"]
+    subjectSelect = ttk.Combobox(subjectFont, textvariable=subjectVar, values=options, state="readonly")
 
-    # Create a variable to store the selected option
-    selected_option = tk.StringVar()
+    # buttonAllUsers = tk.Checkbutton(window, text="Select All Users", font=("Arial", 12), command=selectAll)
 
-    # Create the option menu widget
-    option_menu = tk.OptionMenu(window, selected_option, *options)
+    button1 = tk.Checkbutton(window, text="Open and close left or right fist", font=("Arial", 12))
+    button2 = tk.Checkbutton(window, text="Imagine opening and closing Left or right fist", font=("Arial", 12))
+    button3 = tk.Checkbutton(window, text="Open and close both fists or both feet", font=("Arial", 12))
+    button4 = tk.Checkbutton(window, text="Imagine opening and closing both fists or both feet", font=("Arial", 12))
 
-    # Add the option menu to the main window
-    option_menu.pack()
-
-    # Create a function to print the selected option
-    def print_option():
-        print(selected_option.get())
-
-    # Create a button to trigger the print function
-    buttonOption = tk.Button(window, text="Print Option", command=print_option)
-    
     # Create a button widget
-    button = tk.Button(window, text="Train")
-    button1 = tk.Button(window, text="Analyse")
-
+    buttonTrain = tk.Button(window, text="Train")
+    buttonAnalyse = tk.Button(window, text="Analyse")
+    
     # Add the button to the main window
-    button.pack()
+    subjectSelect.pack()
     button1.pack()
-    buttonOption.pack()
-
+    button2.pack()
+    button3.pack()
+    button4.pack()
+    buttonTrain.pack()
+    buttonAnalyse.pack()
 
     # Start the main event loop
     window.mainloop()
