9b6498343c942a65ccd278ea951b2d4398f80397
Tonou <tonou@tonous-mbp.home>
2023-05-15 02:21:55 +0200

    exp

--- experience.py
+++ experience.py
@@ -2,21 +2,17 @@ experience = [
     {
         "description": "open and close left or right fist",
         "runs": [3, 7, 11],
-        "mapping": {0: "rest", 1: "left fist", 2: "right fist"},
     },
     {
         "description": "imagine opening and closing left or right fist",
         "runs": [4, 8, 12],
-        "mapping": {0: "rest", 1: "imagine left fist", 2: "imagine right fist"},
     },
     {
         "description": "open and close both fists or both feet",
         "runs": [5, 9, 13],
-        "mapping": {0: "rest", 1: "both fists", 2: "both feets"},
     },
     {
         "description": "imagine opening and closing both fists or both feet",
         "runs": [6, 10, 14],
-        "mapping": {0: "rest", 1: "imagine both fists", 2: "imagine both feets"},
     },
 ]
