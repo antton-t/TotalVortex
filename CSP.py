7804945f8f8a68b604145952d7b66b7630b8a73b
Antoine TON THAT <antton-t@student.42.fr>
2023-06-02 09:10:16 +0200

    train is good need to improve csp

--- CSP.py
+++ CSP.py
@@ -3,41 +3,76 @@ import numpy as np
 from sklearn.base import TransformerMixin, BaseEstimator
 from scipy import linalg
 
+# https://github.com/mne-tools/mne-python/blob/maint/1.4/mne/decoding/csp.py#L21-L621
+
 
 class CSP(TransformerMixin, BaseEstimator):
-
     def __init__(
-        self,
-        n_components=4,
-    ):
+            self,
+            n_components=4,
+        ):
+            if not isinstance(n_components, int):
+                raise ValueError('n_components must be an integer.')
+        
+            self.n_components =  n_components
+    
+    def _compute_covariance_matrices(self, X, y):
+        _, n_channels, _ = X.shape
 
-        # Init default CSP
-        if not isinstance(n_components, int):
-            raise ValueError("n_components must be an integer.")
-        self.n_components = n_components
+        covs = []
+        for cur_class in self.classes_:
+            # Concatenate epochs before computing the covariance.
+            x_class = X[y == cur_class]
+            x_class = np.transpose(x_class, [1, 0, 2])
+            x_class = x_class.reshape(n_channels, -1)
+            cov_mat = np.cov(x_class)
+            covs.append(cov_mat)
 
-    def _check_Xy(self, X, y=None):
-
-        if not isinstance(X, np.ndarray):
-            raise ValueError("X should be of type ndarray (got %s)." % type(X))
-        if y is not None:
-            if len(X) != len(y) or len(y) < 1:
-                raise ValueError("X and y must have the same length.")
-        if X.ndim < 3:
-            raise ValueError("X must have at least 3 dimensions.")
+        return np.stack(covs)
 
     def fit(self, X, y):
+        """Estimate the CSP decomposition on epochs."""
 
-        self._check_Xy(X, y)
+        self.classes_ = np.unique(y)
 
-        self._classes = np.unique(y)
-        n_classes = len(self._classes)
-        if n_classes < 2:
-            raise ValueError("n_classes must be >= 2.")
-        if n_classes > 2 and self.component_order == "alternate":
-            raise ValueError(
-                "component_order='alternate' requires two "
-                "classes, but data contains {} classes; use "
-                "component_order='mutual_info' "
-                "instead.".format(n_classes)
-            )
+        # Compute the covariance matrices for each class
+        covs = self._compute_covariance_matrices(X, y)
+
+        # Perform eigenvalue decomposition on the average covariance matrix
+        eigen_values, eigen_vectors = linalg.eigh(covs[0], covs.sum(0))
+
+        # Sort the eigenvalues in descending order
+        ix = np.argsort(np.abs(eigen_values - 0.5))[::-1]
+        eigen_vectors = eigen_vectors[:, ix]
+
+        # Store the CSP filters
+        self.filters_ = eigen_vectors.T
+        pick_filters = self.filters_[:self.n_components]
+
+        # Apply CSP filtering to the input epochs
+        X = np.asarray([np.dot(pick_filters, epoch) for epoch in X])
+
+        # Logarithm of the variance of CSP-filtered signals
+        X = np.log(np.mean(X ** 2, axis=2))
+
+        return self
+
+    def transform(self, X):
+        pick_filters = self.filters_[:self.n_components]
+
+        # Apply CSP filtering to the input epochs
+        X = np.asarray([np.dot(pick_filters, epoch) for epoch in X])
+
+        # Logarithm of the variance of CSP-filtered signals
+        X = np.log(np.mean(X ** 2, axis=2))
+
+        # Zero-mean and unit variance normalization
+        X -= np.mean(X)
+        X /= np.std(X)
+
+        return X
+
+    def fit_transform(self, X, y):
+        # Fit CSP on the training data and apply CSP filtering
+        self.fit(X, y)
+        return self.transform(X)
--- train.py
+++ train.py
@@ -3,11 +3,12 @@ import mne
 import numpy as np
 from sklearn.model_selection import ShuffleSplit, cross_val_score, train_test_split
 from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
-from mne.decoding import CSP
+# from mne.decoding import CSP
 import matplotlib.pyplot as plt
 import os
 import joblib
 
+from CSP import CSP
 from experience import experience
 from bad_channel import dropBadChannel
 from data import getData
@@ -28,7 +29,7 @@ def train(subject:int, exp:int) ->int:
     raw, events = getData(subject=(int(subject)), runs=runs)
 
     # Apply band-pass filter
-    raw.filter(7.0, 30.0, fir_design="firwin", skip_by_annotation="edge")
+    raw.filter(7.0, 40.0, fir_design="firwin", skip_by_annotation="edge")
 
     picks = mne.pick_types(raw.info, meg=False, eeg=True, stim=False, eog=False, exclude='bads')
     
@@ -43,47 +44,30 @@ def train(subject:int, exp:int) ->int:
     epochs_data = epochs.get_data()
     epochs_data_train = epochs_train.get_data()
     cv = ShuffleSplit(10, test_size=0.8, random_state=42)
-    cv_split = cv.split(epochs_data_train)
-
-    # Assemble a classifier
+    
+    csp = CSP(n_components=6)
     lda = LinearDiscriminantAnalysis()
-    csp = CSP(n_components=7, reg=None, log=True, norm_trace=False)
-
-    # Use scikit-learn Pipeline with cross_val_score function
     clf = Pipeline([("CSP", csp), ("LDA", lda)])
-    scores = cross_val_score(clf, epochs_data_train, labels, cv=cv, n_jobs=None)
+
+    # fit our pipeline to the experiment
+    X_train, _, Y_train, _ = train_test_split(epochs_data_train, labels, random_state=0)
+    print(type(epochs_data_train))
+    print(type(labels))
+    clf.fit(X_train, Y_train)
+    
+    scores = cross_val_score(clf, epochs_data_train, labels, n_jobs=None)
     scores_mean = np.mean(scores).round(2)
 
     # Printing the results
     class_balance = np.mean(labels == labels[0])
     class_balance = max(class_balance, 1.0 - class_balance)
-    # print("-------------------------")
-    # print("Classification accuracy: %f / Chance level: %f" % (np.mean(scores), class_balance))
-    # print("-------------------------")
-
-    # plot CSP patterns estimated on full data for visualization
-    csp.fit_transform(epochs_data, labels)
-    
-    # Fitting data and labels
-    clf.fit(epochs_data, labels)
-
-    # Fit pipeline to the exp
-    XTrain, XTest, YTrain, YTest = train_test_split(epochs_train, labels, train_size=0.8, random_state=0)
    
-    # print("-----------------")
-    # print(type(epochs_train))
-    # print(type(labels))
-    # print("-----------------")
-    # Fit the data
-    clf.fit(epochs_data, labels)
-
     # Save classifier
     save_path = SAVE_PATH + "/sub" + str(subject) + "/exp" + str(exp)
-    print("++++++++++++++++++++++++++++++++")
-    print(save_path)
-    print("++++++++++++++++++++++++++++++++")
 
     os.makedirs(save_path, exist_ok=True)
     joblib.dump(clf, save_path + str(exp), compress=0, protocol=None, cache_size=None)
+
+    print("Cross ValScore " + str(scores_mean))
  
     return 0
